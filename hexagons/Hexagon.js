795c3750891523d666afc513b75765e15646e7ab
Devi Acharya <dacharya64@gmail.com>
2019-05-01 15:10:54 -0700

    hexes are now a prefab

---
+++ hexagons/Hexagon.js
@@ -0,0 +1,14 @@
+//hexagon prefab
+function Hexagon(game, key, x, y) {
+	// generate random x,y values
+		Phaser.Sprite.call(this, game, x, y, key);
+
+}
+
+Hexagon.prototype = Object.create(Phaser.Sprite.prototype);
+Hexagon.prototype.constructor = Hexagon;
+
+// override Phaser.Sprite update
+Hexagon.prototype.update = function() {
+
+}
