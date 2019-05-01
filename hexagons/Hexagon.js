48ebfd8709c7a23708c48ca66e3eb828497872ce
Devi Acharya <dacharya64@gmail.com>
2019-05-01 15:12:10 -0700

    auto indent

--- hexagons/Hexagon.js
+++ hexagons/Hexagon.js
@@ -2,7 +2,6 @@
 function Hexagon(game, key, x, y) {
 	// generate random x,y values
 		Phaser.Sprite.call(this, game, x, y, key);
-
 }
 
 Hexagon.prototype = Object.create(Phaser.Sprite.prototype);
--- hexagons/game.js
+++ hexagons/game.js
@@ -7,13 +7,13 @@ window.onload = function() {
 	var gridSizeX = 10;
 	var gridSizeY = 12;
 	var columns = [Math.ceil(gridSizeY/2),Math.floor(gridSizeY/2)];
-     var moveIndex;
-     var sectorWidth = hexagonWidth/4*3;
-     var sectorHeight = hexagonHeight;
-     var gradient = (hexagonWidth/4)/(hexagonHeight/2);
-     var marker;
-     var hexagonGroup;
-     var hexagonArray = [];
+	var moveIndex;
+	var sectorWidth = hexagonWidth/4*3;
+	var sectorHeight = hexagonHeight;
+	var gradient = (hexagonWidth/4)/(hexagonHeight/2);
+	var marker;
+	var hexagonGroup;
+	var hexagonArray = [];
 
 	function onPreload() {
 		game.load.image("hexagon", "hexagon.png");
@@ -23,15 +23,15 @@ window.onload = function() {
 	function onCreate() {
 		hexagonGroup = game.add.group();
 		game.stage.backgroundColor = "#ffffff"
-	     for(var i = 0; i < gridSizeX/2; i ++){
-	          hexagonArray[i] = [];
+		for(var i = 0; i < gridSizeX/2; i ++){
+			hexagonArray[i] = [];
 			for(var j = 0; j < gridSizeY; j ++){
 
 				if(gridSizeX%2==0 || i+1<gridSizeX/2 || j%2==0){
 					var hexagonX = hexagonWidth*i*1.5+(hexagonWidth/4*3)*(j%2);
 					var hexagonY = hexagonHeight*j/2;
 					let hexagon = new Hexagon(game, 'hexagon', hexagonX, hexagonY);
-		    	game.add.existing(hexagon);
+					game.add.existing(hexagon);
 					//var hexagon = game.add.sprite(hexagonX,hexagonY,"hexagon");
 					hexagonGroup.add(hexagon);
 					hexagonArray[i][j]=hexagon;
@@ -39,63 +39,62 @@ window.onload = function() {
 					hexagonText.font = "arial";
 					hexagonText.fontSize = 12;
 					hexagonGroup.add(hexagonText);
-
 				}
 			}
 		}
 		hexagonGroup.y = (game.height-hexagonHeight*Math.ceil(gridSizeY/2))/2;
-          if(gridSizeY%2==0){
-               hexagonGroup.y-=hexagonHeight/4;
-          }
+		if(gridSizeY%2==0){
+			hexagonGroup.y-=hexagonHeight/4;
+		}
 		hexagonGroup.x = (game.width-Math.ceil(gridSizeX/2)*hexagonWidth-Math.floor(gridSizeX/2)*hexagonWidth/2)/2;
-          if(gridSizeX%2==0){
-               hexagonGroup.x-=hexagonWidth/8;
-          }
+		if(gridSizeX%2==0){
+			hexagonGroup.x-=hexagonWidth/8;
+		}
 		marker = game.add.sprite(0,0,"marker");
 		marker.anchor.setTo(0.5);
 		marker.visible=false;
 		hexagonGroup.add(marker);
-          moveIndex = game.input.addMoveCallback(checkHex, this);
-	//add UI text
-	var hexagonInfoText = game.add.text(20,20,"Tile info:")
-	hexagonInfoText.font = "arial";
-	hexagonInfoText.fontSize = 18;
+		moveIndex = game.input.addMoveCallback(checkHex, this);
+		//add UI text
+		var hexagonInfoText = game.add.text(20,20,"Tile info:")
+		hexagonInfoText.font = "arial";
+		hexagonInfoText.fontSize = 18;
 	}
 
-     function checkHex(){
-          var candidateX = Math.floor((game.input.worldX-hexagonGroup.x)/sectorWidth);
-          var candidateY = Math.floor((game.input.worldY-hexagonGroup.y)/sectorHeight);
-          var deltaX = (game.input.worldX-hexagonGroup.x)%sectorWidth;
-          var deltaY = (game.input.worldY-hexagonGroup.y)%sectorHeight;
-          if(candidateX%2==0){
-               if(deltaX<((hexagonWidth/4)-deltaY*gradient)){
-                    candidateX--;
-                    candidateY--;
-               }
-               if(deltaX<((-hexagonWidth/4)+deltaY*gradient)){
-                    candidateX--;
-               }
-          }
-          else{
-               if(deltaY>=hexagonHeight/2){
-                    if(deltaX<(hexagonWidth/2-deltaY*gradient)){
-                         candidateX--;
-                    }
-               }
-               else{
-                    if(deltaX<deltaY*gradient){
-                         candidateX--;
-                    }
-                    else{
-                         candidateY--;
-                    }
-               }
-          }
-          placeMarker(candidateX,candidateY);
-     }
+	function checkHex(){
+		var candidateX = Math.floor((game.input.worldX-hexagonGroup.x)/sectorWidth);
+		var candidateY = Math.floor((game.input.worldY-hexagonGroup.y)/sectorHeight);
+		var deltaX = (game.input.worldX-hexagonGroup.x)%sectorWidth;
+		var deltaY = (game.input.worldY-hexagonGroup.y)%sectorHeight;
+		if(candidateX%2==0){
+			if(deltaX<((hexagonWidth/4)-deltaY*gradient)){
+				candidateX--;
+				candidateY--;
+			}
+			if(deltaX<((-hexagonWidth/4)+deltaY*gradient)){
+				candidateX--;
+			}
+		}
+		else{
+			if(deltaY>=hexagonHeight/2){
+				if(deltaX<(hexagonWidth/2-deltaY*gradient)){
+					candidateX--;
+				}
+			}
+			else{
+				if(deltaX<deltaY*gradient){
+					candidateX--;
+				}
+				else{
+					candidateY--;
+				}
+			}
+		}
+		placeMarker(candidateX,candidateY);
+	}
 
-     function placeMarker(posX,posY){
-     	for(var i = 0; i < gridSizeX/2; i ++){
+	function placeMarker(posX,posY){
+		for(var i = 0; i < gridSizeX/2; i ++){
 			for(var j = 0; j < gridSizeY; j ++){
 				if(gridSizeX%2==0 || i+1<gridSizeX/2 || j%2==0){
 					hexagonArray[i][j].tint = 0xffffff;
